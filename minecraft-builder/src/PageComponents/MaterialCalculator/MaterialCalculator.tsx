749de0776f65d52048ce71ba56289d15a4b7b99a
edhard4542 <edhardin01@gmail.com>
2023-11-11 15:46:24 -0500

    add: added output amount generator

--- minecraft-builder/src/PageComponents/MaterialCalculator/MaterialCalculator.tsx
+++ minecraft-builder/src/PageComponents/MaterialCalculator/MaterialCalculator.tsx
@@ -9,14 +9,16 @@ import { TextField } from "@mui/material";
 export const MaterialCalculator: FC = () => {
   const [selectedConversion, setSelectedConversion] = useState<string>("");
   const [inputMaterialLabel, setInputMaterialLabel] = useState<string>("");
-  const [inputValidationMessage, setInputValidationMessage] = useState<string>("");
+  const [inputValidationMessage, setInputValidationMessage] =
+    useState<string>("");
   const [conversionDetails, setConversionDetails] = useState({
     input: "",
     inputAmount: 0,
     output: "",
     outputAmount: 0,
   });
-
+  const [userInput, setUserInput] = useState<number | null>(null);
+  const conversionKeys: string[] = Object.keys(conversions);
   const handleConversionChange = (event: SelectChangeEvent): void => {
     const selectedValue = event.target.value as string;
     setSelectedConversion(selectedValue);
@@ -27,31 +29,46 @@ export const MaterialCalculator: FC = () => {
       setInputMaterialLabel(`Input material amount for ${inputName}`);
     }
   };
-
-  const conversionKeys: string[] = Object.keys(conversions);
   console.log(conversions, "conversions");
   console.log(selectedConversion, "selectedConversion");
   console.log(conversionKeys, "conversionKeys");
-  console.log(conversionDetails, "conversionDetails"); 
-  console.log(conversionDetails.inputAmount, "conversionDetails"); 
-
-  const handleInputChange = (
-    inputValue: string,
-  ): void => {
+  console.log(conversionDetails, "conversionDetails");
+  console.log(conversionDetails.inputAmount, "conversionDetails");
+  const handleInputChange = (inputValue: string): void => {
     const inputValueParsed = parseFloat(inputValue);
-
+// && = AND
+// || = OR
     if (
-      isNaN(inputValueParsed) ||
-      inputValueParsed < 0 ||
-      inputValueParsed < conversionDetails.inputAmount
+      isNaN(inputValueParsed) || // is not a number
+      inputValueParsed < 0 || // is less than zero
+      inputValueParsed < conversionDetails.inputAmount // input is less than minimum input value  needed
     ) {
       // Invalid input, you can display an error message or take other actions
-      console.error("Invalid input"); 
-      setInputValidationMessage("Please enter the minimum amount"); 
+      console.error("Invalid input");
+      setInputValidationMessage("Please enter the minimum amount");
+      setUserInput(null);
     } else {
       // Valid input, you can handle it accordingly
       console.log("Valid input:", inputValueParsed);
-      setInputValidationMessage(""); 
+      setInputValidationMessage("");
+      setUserInput(inputValueParsed);
+    }
+  };
+
+  const calculateOutput = (): string => {
+    if (userInput === null) {
+      return "";
+    }
+
+    const totalOutput =
+      Math.floor(userInput / conversionDetails.inputAmount) *
+      conversionDetails.outputAmount;
+    const remainder = userInput % conversionDetails.inputAmount;
+
+    if (remainder > 0) {
+      return `${totalOutput} ${conversionDetails.output} with ${remainder} ${conversionDetails.input} remaining`;
+    } else {
+      return `${totalOutput} ${conversionDetails.output}`;
     }
   };
 
@@ -78,16 +95,20 @@ export const MaterialCalculator: FC = () => {
         {selectedConversion && <p>Selected Conversion: {selectedConversion}</p>}
       </div>
       <TextField
+        disabled={selectedConversion.length < 1}
         id="outlined-number"
         label={inputMaterialLabel}
         type="number"
-        onChange={(event: ChangeEvent<HTMLInputElement>) => handleInputChange(event.target.value)}
+        onChange={(event: ChangeEvent<HTMLInputElement>) =>
+          handleInputChange(event.target.value)
+        }
         inputProps={{
           min: selectedConversion ? conversionDetails.inputAmount : 0,
         }}
         error={inputValidationMessage.length > 0}
         helperText={inputValidationMessage.length ? inputValidationMessage : ""}
       />
+      {userInput !== null && <p>{calculateOutput()}</p>}
     </>
   );
 };
