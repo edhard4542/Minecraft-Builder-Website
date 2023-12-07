f3717deb9931d529d102066888a64a8fe3776dbd
Travis Simmons <tsimmons2796@gmail.com>
2023-12-06 22:23:17 -0500

    fix: fixed avatar logout button placement

--- minecraft-builder/src/Navigation/Navigation.tsx
+++ minecraft-builder/src/Navigation/Navigation.tsx
@@ -45,10 +45,10 @@ const Navigation = () => {
 
   const StickyNavBar = styled("div")({
     position: "sticky",
+    display: "flex",
     top: 0,
     zIndex: 1000, // Adjust the z-index as needed
     width: "100%",
-    backgroundColor: "#F9B795",
   });
 
   const handleAvatarClick = (event: MouseEvent<HTMLElement>) => {
@@ -63,143 +63,162 @@ const Navigation = () => {
   };
 
   return (
-    <StickyNavBar>
-      <NavBar>
-        <Tabs
-          value={false}
-          indicatorColor="primary"
-          textColor="primary"
-          centered
+    <Box sx={{ display: "flex", backgroundColor: "#F9B795", width: "100%" }}>
+      <StickyNavBar>
+        <NavBar>
+          <Tabs
+            value={false}
+            indicatorColor="primary"
+            textColor="primary"
+            centered
+            sx={{
+              "& .MuiTab-root": {
+                textTransform: "none",
+                color: "#061E7F",
+                fontSize: "16px",
+              },
+              "& .Mui-selected": {
+                color: "#fff",
+                backgroundColor: "#061E7F",
+              },
+            }}
+          >
+            <Tab
+              label={
+                <Link
+                  to="/home"
+                  style={{ textDecoration: "none", color: "inherit" }}
+                >
+                  Home
+                </Link>
+              }
+            />
+            <Tab
+              label={
+                <Link
+                  to="/recipes"
+                  style={{ textDecoration: "none", color: "inherit" }}
+                >
+                  Crafting Recipes
+                </Link>
+              }
+            />
+
+            <Tab
+              label={
+                <Link
+                  to="/material-calculator"
+                  style={{ textDecoration: "none", color: "inherit" }}
+                >
+                  Material Calculator
+                </Link>
+              }
+            />
+            <Tab
+              label={
+                <Link to="/signed-up-users" style={{ textDecoration: "none" }}>
+                  Signed Up Users
+                </Link>
+              }
+            />
+          </Tabs>
+
+          {/* Search bar */}
+          <SearchContainer>
+            <SearchIcon />
+            <SearchInput placeholder="Search..." />
+          </SearchContainer>
+        </NavBar>
+      </StickyNavBar>
+      <>
+        <Box
           sx={{
-            "& .MuiTab-root": {
-              textTransform: "none",
-              color: "#061E7F",
-              fontSize: "16px",
-            },
-            "& .Mui-selected": {
-              color: "#fff",
-              backgroundColor: "#061E7F",
-            },
+            width: "100%",
+            display: "flex",
+            justifyContent: "flex-end",
+            paddingRight: "16px",
           }}
         >
-          <Tab
-            label={
-              <Link
-                to="/home"
-                style={{ textDecoration: "none", color: "inherit" }}
+          {loggedInUser && (
+            <>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  textAlign: "center",
+                }}
               >
-                Home
-              </Link>
-            }
-          />
-          <Tab
-            label={
-              <Link
-                to="/recipes"
-                style={{ textDecoration: "none", color: "inherit" }}
-              >
-                Crafting Recipes
-              </Link>
-            }
-          />
-
-          <Tab
-            label={
-              <Link
-                to="/material-calculator"
-                style={{ textDecoration: "none", color: "inherit" }}
-              >
-                Material Calculator
-              </Link>
-            }
-          />
-          {/* <Tab
-            label={
-              <Link to="/signed-up-users" style={{ textDecoration: "none" }}>
-                Signed Up Users
-              </Link>
-            }
-          /> */}
-        </Tabs>
-
-        {/* Search bar */}
-        <SearchContainer>
-          <SearchIcon />
-          <SearchInput placeholder="Search..." />
-        </SearchContainer>
-        {loggedInUser && (
-          <>
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                textAlign: "center",
-              }}
-            >
-              <Tooltip title="User Avatar">
-                <IconButton
-                  onClick={handleAvatarClick}
-                  size="small"
-                  sx={{ ml: 2 }}
-                  aria-controls={open ? "account-menu" : undefined}
-                  aria-haspopup="true"
-                  aria-expanded={open ? "true" : undefined}
-                >
-                  <Avatar sx={{ width: 32, height: 32 }}>
-                    {loggedInUser.firstName[0]}
-                  </Avatar>
-                </IconButton>
-              </Tooltip>
-            </Box>
-            <Menu
-              anchorEl={anchorEl}
-              id="account-menu"
-              open={open}
-              onClose={handleClose}
-              onClick={handleClose}
-              PaperProps={{
-                elevation: 0,
-                sx: {
-                  overflow: "visible",
-                  filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-                  mt: 1.5,
-                  "& .MuiAvatar-root": {
-                    width: 32,
-                    height: 32,
-                    ml: -0.5,
-                    mr: 1,
+                <Tooltip title="User Avatar">
+                  <IconButton
+                    onClick={handleAvatarClick}
+                    size="small"
+                    sx={{ ml: 2 }}
+                    aria-controls={open ? "account-menu" : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? "true" : undefined}
+                  >
+                    <Avatar sx={{ width: 32, height: 32 }}>
+                      {loggedInUser.firstName[0]}
+                    </Avatar>
+                  </IconButton>
+                </Tooltip>
+              </Box>
+              <Menu
+                anchorEl={anchorEl}
+                id="account-menu"
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                PaperProps={{
+                  elevation: 0,
+                  sx: {
+                    overflow: "visible",
+                    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                    mt: 1.5,
+                    "& .MuiAvatar-root": {
+                      width: 32,
+                      height: 32,
+                      ml: -0.5,
+                      mr: 1,
+                    },
+                    "&:before": {
+                      content: '""',
+                      display: "block",
+                      position: "absolute",
+                      top: 0,
+                      right: 14,
+                      width: 10,
+                      height: 10,
+                      bgcolor: "background.paper",
+                      transform: "translateY(-50%) rotate(45deg)",
+                      zIndex: 0,
+                    },
                   },
-                  "&:before": {
-                    content: '""',
-                    display: "block",
-                    position: "absolute",
-                    top: 0,
-                    right: 14,
-                    width: 10,
-                    height: 10,
-                    bgcolor: "background.paper",
-                    transform: "translateY(-50%) rotate(45deg)",
-                    zIndex: 0,
-                  },
-                },
-              }}
-              transformOrigin={{ horizontal: "right", vertical: "top" }}
-              anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-            >
-              <MenuItem onClick={handleLogout}>
-                <ListItemIcon>
-                  <Logout fontSize="small" />
-                </ListItemIcon>
-                Logout
-              </MenuItem>
-            </Menu>
-          </>
-        )}
-        {!loggedInUser && (
-          <UserAccountButton setLoggedInUser={setLoggedInUser} />
-        )}
-      </NavBar>
-    </StickyNavBar>
+                }}
+                anchorOrigin={{
+                  vertical: "bottom", // Menu is placed below the anchorEl
+                  horizontal: "right", // Menu is aligned to the right of the anchorEl
+                }}
+                transformOrigin={{
+                  vertical: "top", // The top of the Menu is aligned with the bottom of the anchorEl
+                  horizontal: "right", // The right of the Menu is aligned with the right of the anchorEl
+                }}
+              >
+                <MenuItem onClick={handleLogout}>
+                  <ListItemIcon>
+                    <Logout fontSize="small" />
+                  </ListItemIcon>
+                  Logout
+                </MenuItem>
+              </Menu>
+            </>
+          )}
+          {!loggedInUser && (
+            <UserAccountButton setLoggedInUser={setLoggedInUser} />
+          )}
+        </Box>
+      </>
+    </Box>
   );
 };
 
